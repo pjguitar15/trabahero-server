5c457aea54c840b62cac9278bde0e759e0329099
Phil Josol <pjguitar15@gmail.com>
2025-02-02 04:40:38 +0800

    initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Post('register')
+  async register(@Body() body) {
+    return this.usersService.createUser(
+      body.username,
+      body.password,
+      body.confirmPassword,
+    );
+  }
+}
